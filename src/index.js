580d5091b41a1a37773e48708e8c848fe91511b0
gloryarz <gloryarz@gmail.com>
2018-06-17 10:36:27 -0500

    Cambios

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 let imprimir = document.getElementById('imprimir');
+/*
+if (window.attachEvent){
+  document.getElementById('encodeBtn').addEventListener("click, btn1, false")
+} else if (window.attachEvent) {
+
+}
+
+
+
+const prueba = () => {
+  let traeTexto = document.getElementById('txt');
+
+}*/
